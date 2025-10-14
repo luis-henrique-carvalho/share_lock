237ab10df185e7787f91106266398ed34ef1266b
luis-henrique-carvalho <luisdev9225@gmail.com>
2025-10-13 23:31:19 -0300

    Refactor RubyUI components in home view to use new structure; integrate tailwindcss-animate and update importmap for Tailwind CSS plugins.

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,5 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+pin "tailwindcss-animate" # @1.0.7
+pin "tailwindcss/plugin", to: "tailwindcss--plugin.js" # @4.1.14
